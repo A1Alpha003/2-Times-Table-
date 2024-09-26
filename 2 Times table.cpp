4a4c377395774a81dc9a17706a2ed97b9757465e
A1_Alpha <164620731+A1Alpha003@users.noreply.github.com>
2024-09-26 08:32:58 +0500

    Add files via upload

---
+++ 2 Times table.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <windows.h>
+#include <MMsystem.h>
+
+using namespace std;
+
+int main() {
+
+	int i = 1;
+
+	cout << "2 x " << i << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x1.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 1 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x2.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 2 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x3.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 3 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x4.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 4 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x5.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 5 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x6.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 6 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x7.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 7 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x8.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 8 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x9.wav"), NULL, SND_SYNC);
+	cout << "2 x " << i + 9 << "= " << 2 * i << "\n";
+	PlaySound(TEXT("C:\\Users\\anas_\\OneDrive\\Documents\\CYS-1-A\\PF CYS-A\\Dr. Kashif\\2 Times table\\2x10.wav"), NULL, SND_SYNC);
+
+
+}
